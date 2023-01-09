8246e4409bf213ef54c2750881ce3bb2f24e6049
Bryan Leonardo Calix <bcalixvelasquez@gmail.com>
2023-01-08 20:10:30 -0600

    Renombrar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BarraLateralComponent } from './componenst/barra-lateral/barra-lateral.component';
-import { NavbarComponent } from './componenst/navbar/navbar.component';
-import { InicioComponent } from './componenst/inicio/inicio.component';
+import { BarraLateralComponent } from './components/barra-lateral/barra-lateral.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { InicioComponent } from './components/inicio/inicio.component';
 import { ContadorComponent } from './screens/contador/contador.component';
 
 @NgModule({
